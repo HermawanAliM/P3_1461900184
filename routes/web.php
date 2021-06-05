13a8c92a35e90bb6910942816f7b93a4608cd07b
HermawanAliM <hermawanali359@gmail.com>
2021-06-05 14:51:27 +0700

    upload file ke github

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dokter0184', 'App\Http\Controllers\DokterController@index');
+Route::resource('dokter',DokterController::class);
